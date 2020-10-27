d225ea9b8617a5a946828ebd21f409bc508264a8
Daniel Brooks <dbrooks@getpocket.com>
2020-10-27 13:32:21 -0700

    [BACK-356] Setup CDK Base (#7)

---
+++ .aws/src/config/index.ts
@@ -0,0 +1,22 @@
+const name = 'ExploreTopics';
+let environment;
+let domain;
+
+if (process.env.NODE_ENV === 'development') {
+  environment = 'Dev';
+  domain = 'explore-topics.getpocket.dev';
+} else {
+  environment = 'Prod';
+  domain = 'explore-topics.readitlater.com';
+}
+
+export const config = {
+  name,
+  prefix: `${name}-${environment}`,
+  environment,
+  domain,
+  tags: {
+      service: name,
+      environment
+  }
+};
