c41ca36e325bb65b073b8104f2c4aa3c255685a3
Daniel Brooks <bass_rock@me.com>
2020-10-27 14:59:29 -0700

    setup mock dynamodb data

--- .docker/localstack/dynamodb.sh
+++ .docker/localstack/dynamodb.sh
@@ -7,6 +7,8 @@ TABLE_DEFINITIONS=(
 )
 
 for json_file in "${TABLE_DEFINITIONS[@]}"; do
+  #start fresh and delete the table if it exists
+  awslocal dynamodb delete-table --table-name ${json_file} || true
   awslocal dynamodb create-table --cli-input-json file://$(dirname "${BASH_SOURCE[0]}")/dynamodb/${json_file}.json
   awslocal dynamodb batch-write-item --request-items file://$(dirname "${BASH_SOURCE[0]}")/dynamodb/${json_file}_data.json
 done
