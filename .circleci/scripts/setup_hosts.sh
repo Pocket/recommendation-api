c68feb93c0f2e21929a47726f304080e6f0d6275
Kelvin Yeboah <kyeboah@getpocket.com>
2020-10-29 12:50:58 -0400

    don't need sudo to run

--- .circleci/scripts/setup_hosts.sh
+++ .circleci/scripts/setup_hosts.sh
@@ -8,7 +8,7 @@ echo "Adding service hosts records"
 declare -a arr=("localstack")
 
 for i in "${arr[@]}"; do
-    echo 127.0.0.1 "$i" | sudo tee -a /etc/hosts
+    echo 127.0.0.1 "$i" | tee -a /etc/hosts
 done
 
 
