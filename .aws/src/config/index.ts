27062491cf187c611a995f64bc512608ce98f98e
Daniel Brooks <dbrooks@getpocket.com>
2020-11-03 13:16:59 -0800

    [back-357] adding application creation (#15)

--- .aws/src/config/index.ts
+++ .aws/src/config/index.ts
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 export const config = {
   name,
   prefix: `${name}-${environment}`,
+  shortName: 'EXTOP',
   environment,
   domain,
   tags: {
--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -1,8 +1,9 @@
 import {Construct} from 'constructs';
 import {App, RemoteBackend, TerraformStack} from 'cdktf';
-import {AwsProvider} from '../.gen/providers/aws';
+import {AwsProvider, DataAwsCallerIdentity, DataAwsKmsAlias, DataAwsRegion} from '../.gen/providers/aws';
 import {config} from './config';
 import {DynamoDB} from "./dynamodb";
+import {PocketALBApplication} from "@pocket/terraform-modules";
 
 class ExploreTopics extends TerraformStack {
     constructor(scope: Construct, name: string) {
@@ -22,7 +23,60 @@ class ExploreTopics extends TerraformStack {
             ],
         });
 
+        const region = new DataAwsRegion(this, 'region');
+        const caller = new DataAwsCallerIdentity(this, 'caller');
+        const secretsManager = new DataAwsKmsAlias(this, 'kms_alias', {
+            name: 'alias/aws/secretsmanager'
+        });
+
+
         new DynamoDB(this, 'dynamodb');
+
+        new PocketALBApplication(this, 'application', {
+            internal: true,
+            prefix: config.prefix,
+            alb6CharacterPrefix: config.shortName,
+            tags: config.tags,
+            cdn: false,
+            domain: config.domain,
+            containerConfigs: [
+                {
+                    name: 'app',
+                    hostPort: 8080,
+                    containerPort: 8080,
+                    envVars: [
+                        {
+                            name: 'foo',
+                            value: 'bar',
+                        },
+                    ]
+                }
+            ],
+            exposedContainer: {
+                name: 'app',
+                port: 8080,
+            },
+            ecsIamConfig: {
+                prefix: config.prefix,
+                taskExecutionRolePolicyStatements: [
+                    //This policy could probably go in the shared module in the future.
+                    {
+                        actions: [
+                            'secretsmanager:GetSecretValue',
+                            'kms:Decrypt'
+                        ],
+                        resources: [
+                            `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared`,
+                            `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared/*`,
+                            secretsManager.targetKeyArn
+                        ],
+                        effect: 'Allow'
+                    }
+                ],
+                taskRolePolicyStatements: [],
+                taskExecutionDefaultAttachmentArn: 'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
+            }
+        })
     }
 }
 
