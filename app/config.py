14484cf5cb864f17ca4cdb09e1167d3c3cb3d054
Daniel Brooks <bass_rock@me.com>
2020-11-03 20:58:53 -0800

    adding in sentry

--- app/config.py
+++ app/config.py
@@ -5,3 +5,9 @@ dynamodb = {
     'explore_topics_metadata_table': os.getenv('EXPLORE_TOPICS_METADATA_TABLE', 'explore_topics_metadata'),
     'explore_topics_candidates_table': os.getenv('EXPLORE_TOPICS_CANDIDATES_TABLE', 'explore_topics_candidates')
 }
+
+sentry = {
+    'dsn': os.getenv('SENTRY_DSN', 'https://examplePublicKey@o0.ingest.sentry.io/0'),
+    'release': os.getenv('GIT_SHA', '1234'),
+    'environment': os.getenv('ENVIRONMENT', 'local')
+}
