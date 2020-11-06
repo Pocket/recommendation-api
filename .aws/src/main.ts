4209bafa5750d2fb1606ccb81eb38e4f1cb47c52
Daniel Brooks <bass_rock@me.com>
2020-11-05 16:58:05 -0800

    adding in table names

--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -49,6 +49,18 @@ class ExploreTopics extends TerraformStack {
                             name: 'ENVIRONMENT',
                             value: process.env.NODE_ENV, // this gives us a nice lowercase production and development
                         },
+                        {
+                            name: 'AWS_DYNAMODB_ENDPOINT_URL',
+                            value: `https://dynamodb.${region.name}.amazonaws.com`
+                        },
+                        {
+                            name: 'EXPLORE_TOPICS_METADATA_TABLE',
+                            value: dynamodb.metadataTable.dynamodb.name
+                        },
+                        {
+                            name: 'EXPLORE_TOPICS_CANDIDATES_TABLE',
+                            value: dynamodb.candidatesTable.dynamodb.name
+                        }
                     ]
                 }
             ],
