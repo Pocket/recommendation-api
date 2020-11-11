a829c180da5e1e4911ff3beca66ae4d821ad89b9
Daniel Brooks <bass_rock@me.com>
2020-11-10 17:26:57 -0800

    adding result set limiting

--- app/models/topic_recommendations.py
+++ app/models/topic_recommendations.py
@@ -8,11 +8,11 @@ class TopicRecommendationsModel(BaseModel):
     algorithmic_recommendations: List[RecommendationModel] = []
 
     @staticmethod
-    def get_recommendations(slug: str) -> ['TopicRecommendationsModel']:
+    def get_recommendations(slug: str, algorithmic_count: int, curated_count: int) -> ['TopicRecommendationsModel']:
         topic_recommendations = TopicRecommendationsModel()
         topic_recommendations.algorithmic_recommendations = RecommendationModel.get_recommendations(
             slug=slug,
-            recommendation_type=RecommendationType.ALGORITHMIC
+            recommendation_type=RecommendationType.ALGORITHMIC,
         )
         topic_recommendations.curated_recommendations = RecommendationModel.get_recommendations(
             slug=slug,
@@ -22,13 +22,18 @@ class TopicRecommendationsModel(BaseModel):
         # dedupe items in the algorithmic recommendations
         topic_recommendations = TopicRecommendationsModelUtils.dedupe(topic_recommendations)
 
+        # lets limit the result set to what was requested.
+        topic_recommendations = TopicRecommendationsModelUtils.limit_results(topic_recommendations,
+                                                                             algorithmic_count,
+                                                                             curated_count)
+
         # TODO: Publisher spread module
         return topic_recommendations
 
 
 class TopicRecommendationsModelUtils:
     @staticmethod
-    def dedupe(topic_recs_model: TopicRecommendationsModel) -> ['TopicRecommendationsModel']:
+    def dedupe(topic_recs_model: TopicRecommendationsModel) -> TopicRecommendationsModel:
         # are there dupes?
         curated_item_ids = {x.item_id for x in topic_recs_model.curated_recommendations}
         algorithmic_item_ids = {x.item_id for x in topic_recs_model.algorithmic_recommendations}
@@ -41,3 +46,16 @@ class TopicRecommendationsModelUtils:
                                                                   topic_recs_model.algorithmic_recommendations)
 
         return topic_recs_model
+
+    @staticmethod
+    def limit_results(topic_recommendations: TopicRecommendationsModel,
+                      algorithmic_count: int,
+                      curated_count: int
+                      ) -> TopicRecommendationsModel:
+        # The requester wants us to limit our results, so lets truncate the arrays
+        topic_recommendations.algorithmic_recommendations = topic_recommendations.algorithmic_recommendations[
+                                                            0: algorithmic_count]
+        topic_recommendations.curated_recommendations = topic_recommendations.curated_recommendations[
+                                                        0: curated_count]
+
+        return topic_recommendations
