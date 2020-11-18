f800cf98141d56f37c47fe70c917e67d3f5f4581
Kelvin Yeboah <kyeboah@getpocket.com>
2020-11-18 14:10:27 -0500

    add xray to code

--- app/config.py
+++ app/config.py
@@ -3,6 +3,10 @@ import os
 # as this file executes in the 'app' directory, move up one dir to get to the project root
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/../'
 
+service = {
+    'name': 'ExploreTopics'
+}
+
 dynamodb = {
     'endpoint_url': os.getenv('AWS_DYNAMODB_ENDPOINT_URL', None),
     'explore_topics_metadata_table': os.getenv('EXPLORE_TOPICS_METADATA_TABLE', 'explore_topics_metadata'),
