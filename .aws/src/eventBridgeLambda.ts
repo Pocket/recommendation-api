058b11149e323d114ddac5eb00a6513a31f20f03
Kelvin Yeboah <kyeboah@getpocket.com>
2020-12-16 15:52:04 -0500

    [BACK-373]: alarms and pagerduty (#91)
    * wip: alarms and pagerduty
    
    * add pagerduty and alarms
    
    * add lambda alarms
    
    * use non-critical topics for lambda

--- .aws/src/eventBridgeLambda.ts
+++ .aws/src/eventBridgeLambda.ts
@@ -1,14 +1,20 @@
-import { TerraformStack } from "cdktf";
-import { Construct } from "constructs";
-import { config } from "./config";
-import { ApplicationDynamoDBTable, PocketVPC } from "@pocket/terraform-modules";
-import { PocketEventBridgeWithLambdaTarget } from "@pocket/terraform-modules/dist/src/pocket/PocketEventBridgeWithLambdaTarget";
-import { LAMBDA_RUNTIMES } from "@pocket/terraform-modules/dist/src/base/ApplicationVersionedLambda";
-import { DataAwsSecretsmanagerSecretVersion, DataAwsSsmParameter } from "../.gen/providers/aws";
+import {Resource} from "cdktf";
+import {Construct} from "constructs";
+import {config} from "./config";
+import {ApplicationDynamoDBTable, PocketVPC} from "@pocket/terraform-modules";
+import {PocketEventBridgeWithLambdaTarget} from "@pocket/terraform-modules/dist/src/pocket/PocketEventBridgeWithLambdaTarget";
+import {LAMBDA_RUNTIMES} from "@pocket/terraform-modules/dist/src/base/ApplicationVersionedLambda";
+import {DataAwsSecretsmanagerSecretVersion, DataAwsSsmParameter} from "../.gen/providers/aws";
+import {PocketPagerDuty} from "@pocket/terraform-modules/dist/src/pocket/PocketPagerDuty";
 
 
-export class EventBridgeLambda extends TerraformStack {
-  constructor(private scope: Construct, private name: string, candidatesTable: ApplicationDynamoDBTable) {
+export class EventBridgeLambda extends Resource {
+  constructor(
+    scope: Construct,
+    private name: string,
+    candidatesTable: ApplicationDynamoDBTable,
+    pagerDuty: PocketPagerDuty
+  ) {
     super(scope, name);
 
     const vpc = new PocketVPC(this, 'pocket-shared-vpc');
@@ -17,81 +23,98 @@ export class EventBridgeLambda extends TerraformStack {
 
     new PocketEventBridgeWithLambdaTarget(this, 'translation-event-bridge-lambda', {
       name: `${config.prefix}-Translation`,
-      eventPattern: {
-        "source": [
-          "aws.states"
+      lambda: {
+        runtime: LAMBDA_RUNTIMES.PYTHON38,
+        handler: 'aws_lambda.index.handler',
+        timeout: 30,
+        executionPolicyStatements: [
+          {
+            effect: 'Allow',
+            actions: [
+              'dynamodb:BatchWriteItem',
+              'dynamodb:PutItem',
+              'dynamodb:DescribeTable',
+              'dynamodb:UpdateItem'
+            ],
+            resources: [
+              candidatesTable.dynamodb.arn,
+              `${candidatesTable.dynamodb.arn}/*`
+            ]
+          },
+          {
+            effect: 'Allow',
+            actions: [
+              'secretsmanager:DescribeSecret',
+              'secretsmanager:GetSecretValue'
+            ],
+            resources: [
+              'arn:aws:secretsmanager:*:*:secret:CodeBuild/Metaflow*'
+            ]
+          },
+          {
+            effect: 'Allow',
+            actions: [
+              's3:List*',
+              's3:Get*',
+            ],
+            resources: [
+              'arn:aws:s3:::metaflow*',
+              'arn:aws:s3:::metaflow*/*'
+            ]
+          }
         ],
-        "detail-type": [
-          "Step Functions Execution Status Change"
-        ],
-        "detail": {
-          "status": [
-            "SUCCEEDED"
-          ],
-          "stateMachineArn": config.stateMachines.map(name => {
-            return `arn:aws:states:${vpc.region}:${vpc.accountId}:stateMachine:${name}`
-          })
+        environment: {
+          RECOMMENDATION_API_CANDIDATES_TABLE: candidatesTable.dynamodb.name,
+          SENTRY_DSN: sentryDsn,
+          GIT_SHA: gitSha,
+          ENVIRONMENT: config.environment === 'Prod' ? 'production' : 'development',
+          // We are adding these metaflow required env variables here because metaflow client will not let us set these as
+          // configuration/options at runtime and lambd will not let is set env vars at runtime either. So here we are...
+          METAFLOW_DATASTORE_SYSROOT_S3: `\${jsondecode(${metaflowSecretFqn}.secret_string).METAFLOW_DATASTORE_SYSROOT_S3}`,
+          METAFLOW_SERVICE_INTERNAL_URL: `\${jsondecode(${metaflowSecretFqn}.secret_string).METAFLOW_SERVICE_INTERNAL_URL}`
+        },
+        vpcConfig: {
+          securityGroupIds: vpc.defaultSecurityGroups.ids,
+          subnetIds: vpc.privateSubnetIds
+        },
+        codeDeploy: {
+          region: vpc.region,
+          accountId: vpc.accountId,
+        },
+        alarms: {
+          invocations: {
+            period: 10800, // 3 hours
+            threshold: 1,
+            comparisonOperator: 'LessThanThreshold',
+            actions: [pagerDuty.snsNonCriticalAlarmTopic.arn]
+          },
+          errors: {
+            period: 10800, // 3 hours
+            threshold: 2,
+            actions: [pagerDuty.snsNonCriticalAlarmTopic.arn]
+          }
         }
       },
-      ruleDescription: 'Capture Metaflow Step Functions SUCCEEDED status',
-      runtime: LAMBDA_RUNTIMES.PYTHON38,
-      handler: 'aws_lambda.index.handler',
-      timeout: 30,
-      executionPolicyStatements: [
-        {
-          effect: 'Allow',
-          actions: [
-            'dynamodb:BatchWriteItem',
-            'dynamodb:PutItem',
-            'dynamodb:DescribeTable',
-            'dynamodb:UpdateItem'
+      eventRule: {
+        pattern: {
+          "source": [
+            "aws.states"
           ],
-          resources: [
-            candidatesTable.dynamodb.arn,
-            `${candidatesTable.dynamodb.arn}/*`
-          ]
+          "detail-type": [
+            "Step Functions Execution Status Change"
+          ],
+          "detail": {
+            "status": [
+              "SUCCEEDED"
+            ],
+            "stateMachineArn": config.stateMachines.map(name => {
+              return `arn:aws:states:${vpc.region}:${vpc.accountId}:stateMachine:${name}`
+            })
+          }
         },
-        {
-          effect: 'Allow',
-          actions: [
-            'secretsmanager:DescribeSecret',
-            'secretsmanager:GetSecretValue'
-          ],
-          resources: [
-            'arn:aws:secretsmanager:*:*:secret:CodeBuild/Metaflow*'
-          ]
-        },
-        {
-          effect: 'Allow',
-          actions: [
-            's3:List*',
-            's3:Get*',
-          ],
-          resources: [
-            'arn:aws:s3:::metaflow*',
-            'arn:aws:s3:::metaflow*/*'
-          ]
-        }
-      ],
-      environment: {
-        RECOMMENDATION_API_CANDIDATES_TABLE: candidatesTable.dynamodb.name,
-        SENTRY_DSN: sentryDsn,
-        GIT_SHA: gitSha,
-        ENVIRONMENT: config.environment === 'Prod' ? 'production' : 'development',
-        // We are adding these metaflow required env variables here because metaflow client will not let us set these as
-        // configuration/options at runtime and lambd will not let is set env vars at runtime either. So here we are...
-        METAFLOW_DATASTORE_SYSROOT_S3: `\${jsondecode(${metaflowSecretFqn}.secret_string).METAFLOW_DATASTORE_SYSROOT_S3}`,
-        METAFLOW_SERVICE_INTERNAL_URL: `\${jsondecode(${metaflowSecretFqn}.secret_string).METAFLOW_SERVICE_INTERNAL_URL}`
-      },
-      vpcConfig: {
-        securityGroupIds: vpc.defaultSecurityGroups.ids,
-        subnetIds: vpc.privateSubnetIds
+        description: 'Capture Metaflow Step Functions SUCCEEDED status',
       },
       tags: config.tags,
-      codeDeploy: {
-        region: vpc.region,
-        accountId: vpc.accountId,
-      }
     });
   }
 
