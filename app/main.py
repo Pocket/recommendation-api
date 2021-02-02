1012f32d3c6003790dd9e7fc79316c91275a4983
Mathijs Miermans <m.miermans@gmail.com>
2021-02-02 10:45:11 -0800

    Added GraphQL Sentry middleware

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 from app.config import sentry as sentry_config
 
 from fastapi import FastAPI, Request
-from starlette.graphql import GraphQLApp
+from app.graphql_app import GraphQLAppWithMiddleware
 from app.graphql.graphql import schema
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -19,19 +19,18 @@ sentry_sdk.init(
     environment=sentry_config['environment'],
     traces_sample_rate=0.1
 )
-sentry_sdk.utils.MAX_STRING_LENGTH = 8192  # undocumented truncation length as of v0.12.2
 
 # Standard asyncio X-Ray configuration, customise as you choose
-#xray_recorder.configure(context=AsyncContext(), service=service.get('domain'))
+xray_recorder.configure(context=AsyncContext(), service=service.get('domain'))
 
 
 app = FastAPI()
+app.add_middleware(BaseHTTPMiddleware, dispatch=xray_middleware)
 app.add_middleware(SentryAsgiMiddleware)
-#app.add_middleware(BaseHTTPMiddleware, dispatch=xray_middleware)
 
 # Add our GraphQL route to the main url
-app.add_route("/", GraphQLApp(schema=schema,
-                              executor_class=AsyncioExecutor))
+graphql_app = GraphQLAppWithMiddleware(schema=schema, executor_class=AsyncioExecutor)
+app.add_route("/", graphql_app)
 
 
 @app.get("/health-check")
