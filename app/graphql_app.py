a4adf485003a60b9b68198a3fc6cfaac2817fadb
Mathijs Miermans <m.miermans@gmail.com>
2021-02-03 08:57:20 -0800

    Add comment

--- app/graphql_app.py
+++ app/graphql_app.py
@@ -12,10 +12,11 @@ class GraphQLSentryMiddleware(object):
 
     def resolve(self, next, root, info, **args):
         promise = next(root, info, **args)
+        # Capture exceptions to Sentry, and reraise such that GraphQL can return a proper error message in the response.
         return promise.then(did_reject=capture_and_reraise)
 
 
-# Starlette has an open PR to support middleware, but it's not merged in yet as of today:
+# Starlette has an open PR to support middleware, but it's not merged in as of today:
 # https://github.com/encode/starlette/pull/1044
 class GraphQLAppWithMiddleware(GraphQLApp):
     def __init__(self, *args, **kwargs):
