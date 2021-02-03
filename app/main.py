42d121508a69560ac9ccc171bced981b8668f713
Mathijs Miermans <m.miermans@gmail.com>
2021-02-02 16:56:51 -0800

    Revert some Sentry debug code

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sentry_sdk
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 from app.config import sentry as sentry_config
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
 from app.graphql.graphql import schema
 from graphql.execution.executors.asyncio import AsyncioExecutor
@@ -23,7 +23,6 @@ sentry_sdk.init(
 # Standard asyncio X-Ray configuration, customise as you choose
 xray_recorder.configure(context=AsyncContext(), service=service.get('domain'))
 
-
 app = FastAPI()
 app.add_middleware(BaseHTTPMiddleware, dispatch=xray_middleware)
 app.add_middleware(SentryAsgiMiddleware)
@@ -32,14 +31,10 @@ app.add_middleware(SentryAsgiMiddleware)
 app.add_route("/", GraphQLApp(schema=schema,
                               executor_class=AsyncioExecutor))
 
+
 @app.get("/health-check")
 async def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/divide-by-zero")
-async def dummy_error():
-    return {"Test": 1/0}
 
 
 if __name__ == "__main__":
