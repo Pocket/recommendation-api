65033a16408b5f33d2ad2aecc6f725c954c819e3
Kelvin Yeboah <kyeboah@getpocket.com>
2021-02-05 13:34:44 -0500

    [BACK-600] Stub out response for getSlate query

--- app/graphql/graphql.py
+++ app/graphql/graphql.py
@@ -2,9 +2,11 @@ from graphene import ObjectType, String, Field, List, Schema, Int
 
 from app.models.topic import TopicModel
 from app.models.topic_recommendations import TopicRecommendationsModel
+from app.models.slate import SlateModel
 
 from app.graphql.topic import Topic
 from app.graphql.topic_recommendations import TopicRecommendations
+from app.graphql.slate import Slate
 
 
 class Query(ObjectType):
@@ -14,16 +16,20 @@ class Query(ObjectType):
                                       algorithmic_count=Int(default_value=20, description="Number of algorithmic "
                                                                                           "results to return"),
                                       curated_count=Int(default_value=5, description="Number of curated "
-                                                                                      "results to return"))
+                                                                                     "results to return"))
     list_topics = List(Topic)
+    get_slate = Field(Slate, slate_id=String(required=True, description="Slate id to get a specific slate"))
 
     async def resolve_get_topic_recommendations(self, info, slug: str, algorithmic_count: int,
-                                          curated_count: int) -> TopicRecommendations:
+                                                curated_count: int) -> TopicRecommendations:
         return await TopicRecommendationsModel.get_recommendations(slug=slug, algorithmic_count=algorithmic_count,
-                                                             curated_count=curated_count)
+                                                                   curated_count=curated_count)
 
     async def resolve_list_topics(self, info) -> [TopicModel]:
         return await TopicModel.get_all()
+
+    async def resolve_get_slate(self, info, slate_id: str) -> SlateModel:
+        return await SlateModel.get_slate(slate_id=slate_id)
 
 
 ##
