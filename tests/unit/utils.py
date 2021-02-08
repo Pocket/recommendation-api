0890599cf5b3d4ffe97c13aa19b7ce88f97e32d2
Kelvin Yeboah <kyeboah@getpocket.com>
2021-02-08 16:06:34 -0500

    [BACK-584] Implement and test rankers

---
+++ tests/unit/utils.py
@@ -0,0 +1,16 @@
+import random
+from typing import List
+from app.models.recommendation import RecommendationModel
+
+
+def generate_recommendations(item_ids: List[int or str]) -> List[RecommendationModel]:
+    recs = []
+    for item_id in item_ids:
+        rec = RecommendationModel()
+        rec.item_id = item_id
+        rec.feed_id = random.randint(0, 101)
+        rec.feed_item_id = random.randint(0, 101)
+        rec.rec_src = 'bowling'
+        recs.append(rec)
+
+    return recs
