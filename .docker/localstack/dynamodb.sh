362e1a1114afa38d27ab5cd2eac538550a80f7e4
Kelvin Yeboah <kyeboah@getpocket.com>
2021-02-08 09:08:08 -0500

    [BACK-572] Fix creating CandidateSets table locally and loading data (#134)
    Co-authored-by: Chelsea Troy <chelseatroy@users.noreply.github.com>

--- .docker/localstack/dynamodb.sh
+++ .docker/localstack/dynamodb.sh
@@ -5,6 +5,7 @@ TABLE_DEFINITIONS=(
   'recommendation_api_metadata'
   'recommendation_api_candidates'
   'recommendation_api_clickdata'
+  'recommendation_api_candidate_sets'
 )
 
 for json_file in "${TABLE_DEFINITIONS[@]}"; do
@@ -13,5 +14,8 @@ for json_file in "${TABLE_DEFINITIONS[@]}"; do
   awslocal dynamodb create-table --cli-input-json file://$(dirname "${BASH_SOURCE[0]}")/dynamodb/${json_file}.json
   awslocal dynamodb batch-write-item --request-items file://$(dirname "${BASH_SOURCE[0]}")/dynamodb/${json_file}_data.json
 done
+
+# Enable TTL on CandidateSets (recommendation_api_candidate_sets) table to mimic production table
+awslocal dynamodb update-time-to-live --table-name recommendation_api_candidate_sets --time-to-live-specification "Enabled=true, AttributeName=expires_at"
 
 set +x
