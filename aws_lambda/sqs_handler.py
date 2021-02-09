1110e03c1c925fd82ee1f83d87a9417eae519efe
Mathijs Miermans <m.miermans@gmail.com>
2021-02-09 13:40:36 -0800

    Revert debug logging

--- aws_lambda/sqs_handler.py
+++ aws_lambda/sqs_handler.py
@@ -8,12 +8,10 @@ from aws_lambda.config.index import dynamodb as dynamodb_config
 
 def handler(event: Dict[str, Any], context=None):
     records = event['Records']
-    print("records:", records)
 
     dynamodb = boto3.resource('dynamodb', endpoint_url=dynamodb_config.get('endpoint_url'))
     table = dynamodb.Table(dynamodb_config.get('recommendation_api_candidate_sets_table'))
     with table.batch_writer() as batch:
         for record in records:
-            print("candidate_set:", candidate_set)
             candidate_set = json.loads(record['body'])
             batch.put_item(Item=candidate_set)
