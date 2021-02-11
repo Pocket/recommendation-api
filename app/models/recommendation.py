50a46dfc8604af19445917700a5b594ce9bcd7a3
Kelvin Yeboah <kyeboah@getpocket.com>
2021-02-11 16:31:11 -0500

    [BACK-586]: create complete flow to get a layout (#149)

--- app/models/recommendation.py
+++ app/models/recommendation.py
@@ -1,9 +1,16 @@
-from pydantic import BaseModel
 import aioboto3
+
+from pydantic import BaseModel
 from boto3.dynamodb.conditions import Key
-from app.config import dynamodb as dynamodb_config
 from enum import Enum
 from aws_xray_sdk.core import xray_recorder
+from asyncio import gather
+
+from app.config import dynamodb as dynamodb_config
+from app.models.slate_experiment import SlateExperimentModel
+from app.models.candidate_set import CandidateSetModel
+from app.models.clickdata import ClickdataModel, RecommendationModules
+from app.rankers import get_ranker
 
 
 class RecommendationType(Enum):
@@ -37,3 +44,38 @@ class RecommendationModel(BaseModel):
             return []
         # assume 'candidates' below contains publisher
         return list(map(RecommendationModel.dynamodb_candidate_to_recommendation, response['Items'][0]['candidates']))
+
+    @staticmethod
+    async def get_recommendations_from_experiment(experiment: SlateExperimentModel) -> ['RecommendationModel']:
+        candidate_sets = []
+        # for each candidate set id, get the candidate set record from the db
+        for candidate_set_id in experiment.candidate_sets:
+            candidate_sets.append(CandidateSetModel.get(candidate_set_id))
+        candidate_sets = await gather(*candidate_sets)
+
+        recommendations = []
+        # get the recommendations
+        for candidate_set in candidate_sets:
+            for candidate in candidate_set.candidates:
+                recommendations.append(RecommendationModel.parse_obj(candidate))
+
+        # apply rankers from the slate experiment on the candidate set's candidates
+        for ranker in experiment.rankers:
+            if ranker == 'thompson-sampling':
+                # thompson sampling takes two specific arguments so it needs to be handled differently
+                recommendations = await RecommendationModel.__thompson_sample(recommendations, ranker)
+                continue
+            recommendations = get_ranker(ranker)(recommendations)
+
+        return recommendations
+
+    @staticmethod
+    async def __thompson_sample(recommendations, ranker) -> ['RecommendationModel']:
+        item_ids = [recommendation.item_id for recommendation in recommendations]
+        try:
+            click_data = await ClickdataModel.get_clickdata(RecommendationModules.TOPIC, item_ids)
+        except ValueError:
+            rec_item_ids = ','.join(item_ids)
+            print(f'click data not found for candidates with item ids: {rec_item_ids}')
+            click_data = {}
+        return get_ranker(ranker)(recommendations, click_data)
