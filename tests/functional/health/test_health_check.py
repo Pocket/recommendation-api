34a8bd1a6cb9c5007d5d3305c84ff90f10e6f4a0
Mathijs Miermans <m.miermans@gmail.com>
2021-02-11 20:44:36 -0800

    Added functional test

---
+++ tests/functional/health/test_health_check.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+from app.health_status import set_health_status, HealthStatus
+
+
+class TestHealthCheck:
+    client: TestClient = TestClient(app)
+
+    def test_unknown_status(self):
+        set_health_status(HealthStatus.UNKNOWN)
+        response = self.client.get("/health-check")
+        assert response.status_code == 503
+        assert response.json() == {"status": "UNKNOWN"}
+
+    def test_unhealthy_status(self):
+        set_health_status(HealthStatus.UNHEALTHY)
+        response = self.client.get("/health-check")
+        assert response.status_code == 503
+        assert response.json() == {"status": "UNHEALTHY"}
+
+    def test_healthy_status(self):
+        set_health_status(HealthStatus.HEALTHY)
+        response = self.client.get("/health-check")
+        assert response.status_code == 200
+        assert response.json() == {"status": "HEALTHY"}
