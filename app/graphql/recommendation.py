be8cba25524ffdd263f64ae3c8b3528a31539e65
Kelvin Yeboah <kyeboah@getpocket.com>
2021-02-19 11:21:36 -0500

    fix(schema): update Recommendation model schema and key

--- app/graphql/recommendation.py
+++ app/graphql/recommendation.py
@@ -1,16 +1,10 @@
 from graphene_pydantic import PydanticObjectType
-from graphene import NonNull, String
 from graphene_federation import key
 
 from app.models.recommendation import RecommendationModel
-from app.graphql.item import Item
 
 
-@key('recSrc')
+@key('item { itemId }')
 class Recommendation(PydanticObjectType):
-    item_id = NonNull(String)
-    item = NonNull(Item)
-    rec_src = NonNull(String)
-
     class Meta:
         model = RecommendationModel
