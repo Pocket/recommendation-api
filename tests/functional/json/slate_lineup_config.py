3ae6be3b835b6e3b81a8c89f105d260130260912
Mathijs Miermans <mmiermans@users.noreply.github.com>
2021-03-12 15:38:56 -0800

    [SHIPIT-123] Fix topic slate order (#214)
    * Fix topic slate order
    
    * Add test for explore topic lineups
    
    * Fix coronavirus order
    
    * Add comments to test
    
    * Assert test matches topics

--- tests/functional/json/slate_lineup_config.py
+++ tests/functional/json/slate_lineup_config.py
@@ -1,3 +1,5 @@
+import re
+
 from app.models.slate_lineup_experiment import SlateLineupExperimentModel
 from app.models.slate_config import SlateConfigModel
 from app.models.slate_lineup_config import SlateLineupConfigModel, validate_unique_guids
@@ -12,7 +14,7 @@ def test_validate_config():
         validate_unique_guids(lineup_config, slate_config)
 
 
-def test_slate_lineup_config():
+def test_slate_exists():
     """
     Test that slate and lineup configs are valid
     """
@@ -27,3 +29,34 @@ def test_slate_lineup_config():
             for slate in experiment.slates:
                 if not SlateLineupExperimentModel.slate_id_exists(slate):
                     raise ValueError(f'slate {slate_lineup_config.id}|{experiment.description}|{slate} was not found')
+
+
+def test_explore_topic_slates():
+    """
+    Test that all Explore Topic lineups have a curated and algorithmic slate, in this order.
+
+    The Web repo currently relies on this ordering in src/Discover/RecommendationAPIClient.php:getTopicRecommendations
+    to return curated and algorithmic slates via the legacy v3/discover/topics endpoint.
+    """
+    slate_configs_by_id = {s.id: s for s in SlateConfigModel.load_slate_configs()}
+    slate_lineup_configs = SlateLineupConfigModel.load_slate_lineup_configs()
+
+    match_count = 0
+
+    for slate_lineup_config in slate_lineup_configs:
+        match = re.match(r'^Explore (.*?) Topic SlateLineup$', slate_lineup_config.description)
+        if match:
+            match_count += 1
+            topic_name = match.group(1)
+            for experiment in slate_lineup_config.experiments:
+                slates = [slate_configs_by_id[slate_id] for slate_id in experiment.slates]
+
+                # Assert that the first two slates are topic slates
+                assert topic_name in slates[0].displayName
+                assert topic_name in slates[1].displayName
+
+                # Assert that slates are ordered curated, algorithmic.
+                assert "Curated" in slates[0].displayName
+                assert "Algorithmic" in slates[1].displayName
+
+    assert match_count == 15
