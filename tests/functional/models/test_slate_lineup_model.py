9a502d7a3a2ccf51e261aa6fe7bb1486d9530be4
Mathijs Miermans <m.miermans@gmail.com>
2021-03-23 14:33:46 -0700

    Add functional test for get_slate_lineup

--- tests/functional/models/test_slate_lineup_model.py
+++ tests/functional/models/test_slate_lineup_model.py
@@ -8,14 +8,34 @@ from app.models.slate_experiment import SlateExperimentModel
 from app.models.slate_lineup_experiment import SlateLineupExperimentModel
 from app.models.slate_lineup_config import SlateLineupConfigModel
 
-slate_lineup_config_id = 'test-slate_lineup-config-id'
-slate_lineup_experiment = SlateLineupExperimentModel('test-ex', 'test-ex-desc', ['top15'], ['test-slate-id'])
-slate_lineup_config_model = SlateLineupConfigModel(slate_lineup_config_id, 'test-desc', [slate_lineup_experiment])
-
-slate_config_id = 'test-slate_lineup-config-id'
+# First slate
+slate_config_id = 'test-slate-config-id'
 slate_experiment = SlateExperimentModel('test-ex', 'test-ex-desc', ['top15', 'thompson-sampling'],
                                         ['test-candidate-id'])
 slate_config_model = SlateConfigModel(slate_config_id, 'test-this-slate', 'test-desc', [slate_experiment])
+
+# Second slate
+slate_config_id_2 = 'test-slate-config-id-2'
+slate_experiment_2 = SlateExperimentModel('test-ex-2', 'test-ex-desc-2', ['top15', 'thompson-sampling'],
+                                        ['test-candidate-id-2'])
+slate_config_model_2 = SlateConfigModel(slate_config_id_2, 'test-this-slate-2', 'test-desc-2', [slate_experiment_2])
+
+# Lineup with one slate
+slate_lineup_config_id = 'test-slate_lineup-config-id'
+slate_lineup_experiment = SlateLineupExperimentModel('test-ex', 'test-ex-desc', ['top15'], [slate_config_id])
+slate_lineup_config_model = SlateLineupConfigModel(slate_lineup_config_id, 'test-desc', [slate_lineup_experiment])
+
+# Lineup with two slates
+slate_lineup_config_id_2 = 'test-slate_lineup-config-id-2'
+slate_lineup_experiment_2 = SlateLineupExperimentModel('test-ex-2', 'test-ex-desc-2', ['top15'],
+                                                     slates=[slate_config_id, slate_config_id_2])
+slate_lineup_config_model_2 = SlateLineupConfigModel(slate_lineup_config_id_2, 'test-desc', [slate_lineup_experiment_2])
+
+# Slates by id
+slate_configs_by_id = {
+    slate_config_id: slate_config_model,
+    slate_config_id_2: slate_config_model_2,
+}
 
 
 class TestSlateLineupModel(TestDynamoDBBase):
@@ -90,3 +110,50 @@ class TestSlateLineupModel(TestDynamoDBBase):
 
         slate_lineup = await SlateLineupModel.get_slate_lineup(slate_lineup_config_id, slate_count=0)
         assert len(slate_lineup.slates) == 0
+
+    @patch.object(SlateConfigModel, 'SLATE_CONFIGS_BY_ID', slate_configs_by_id)
+    @patch('app.models.slate_lineup_config.SlateLineupConfigModel.find_by_id', return_value=slate_lineup_config_model_2)
+    async def test_get_slate_lineup_dedupe_recs(self, slate_lineup_config):
+        self.candidateSetTable.put_item(Item={
+            "id": "test-candidate-id",
+            "version": 1,
+            "created_at": 1612907252,
+            "candidates": [
+                {
+                    "feed_id": 1,
+                    "item_id": 10,
+                    "publisher": "hbr.org"
+                },
+                {
+                    "feed_id": 1,
+                    "item_id": 11,
+                    "publisher": "hbr.org"
+                },
+            ]
+        })
+
+        self.candidateSetTable.put_item(Item={
+            "id": "test-candidate-id-2",
+            "version": 1,
+            "created_at": 1612907252,
+            "candidates": [
+                {
+                    "feed_id": 1,
+                    "item_id": 11,
+                    "publisher": "hbr.org"
+                },
+                {
+                    "feed_id": 1,
+                    "item_id": 12,
+                    "publisher": "hbr.org"
+                }
+            ]
+        })
+
+        slate_lineup = await SlateLineupModel.get_slate_lineup(slate_lineup_config_id_2)
+        assert len(slate_lineup.slates[0].recommendations) == 2
+        assert len(slate_lineup.slates[1].recommendations) == 1
+
+        assert slate_lineup.slates[0].recommendations[0].item_id == '10'
+        assert slate_lineup.slates[0].recommendations[0].item_id == '11'
+        assert slate_lineup.slates[1].recommendations[0].item_id == '12'
