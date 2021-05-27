15c62e27e280ed7cf9aea8666b4a3ee5c49a29e2
Mathijs Miermans <m.miermans@gmail.com>
2021-05-26 21:22:52 -0700

    Fix aiohttp request tests

--- tests/functional/models/test_personalized_topics.py
+++ tests/functional/models/test_personalized_topics.py
@@ -2,9 +2,7 @@ import json
 import os
 import unittest
 
-import aiohttp
-import responses
-import requests.exceptions
+from aioresponses import aioresponses
 
 import app.config
 from app.models.personalized_topics import PersonalizedTopics
@@ -23,30 +21,33 @@ class TestClickdataModel(unittest.IsolatedAsyncioTestCase):
         assert len(personalized_topics.curator_topics) == 16
         assert personalized_topics.curator_topics[0].curator_topic_label == 'Technology'
 
-    @responses.activate
-    async def test_get(self):
+    @aioresponses()
+    async def test_get(self, mocked):
         user_id = '123'
         url = f'{app.config.recit["endpoint_url"]}/v1/user_profile/{user_id}?predict_topics=true'
         fixture = await self._read_json_asset("recit_full_user_profile.json")
-        responses.add(responses.GET, url, status=200, json=fixture)
+
+        mocked.get(url, status=200, payload=fixture)
 
         personalized_topics = await PersonalizedTopics.get(user_id)
         assert len(personalized_topics.curator_topics) == 16
 
-    @responses.activate
-    async def test_get_404(self):
+    @aioresponses()
+    async def test_get_404(self, mocked):
         user_id = '123'
         url = f'{app.config.recit["endpoint_url"]}/v1/user_profile/{user_id}?predict_topics=true'
-        responses.add(responses.GET, url, status=404)
+
+        mocked.get(url, status=404)
 
         personalized_topics = await PersonalizedTopics.get(user_id)
         assert len(personalized_topics.curator_topics) == 0
 
-    @responses.activate
-    async def test_get_500(self):
+    @aioresponses()
+    async def test_get_500(self, mocked):
         user_id = '123'
         url = f'{app.config.recit["endpoint_url"]}/v1/user_profile/{user_id}?predict_topics=true'
-        responses.add(responses.GET, url, status=500)
+
+        mocked.get(url, status=500)
 
         with self.assertRaises(Exception):
             await PersonalizedTopics.get(user_id)
