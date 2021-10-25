2e82857f39f494b1c4534dc681a48c8a8be8b264
Chelsea Troy <chelsea.dommert@gmail.com>
2021-10-25 18:00:32 -0500

    Force app startup events and populate test db

--- tests/functional/graphql/test_get_slate_lineup.py
+++ tests/functional/graphql/test_get_slate_lineup.py
@@ -1,6 +1,9 @@
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from graphene.test import Client
+from fastapi.testclient import TestClient
+
 from app.graphql.graphql import schema
+from app.main import app, load_slate_configs
 from tests.functional.test_dynamodb_base import TestDynamoDBBase
 
 
@@ -9,37 +12,49 @@ class TestGetSlateLineup(TestDynamoDBBase):
 
     async def asyncSetUp(self):
         await super().asyncSetUp()
-        self.populate_candidate_table()
+        self.populate_candidate_sets_table()
         self.client = Client(schema)
 
     def test_get_slate_lineup(self):
-        executed = self.client.execute('''
-          query GetSlateLineup {
-            getSlateLineup(slateLineupId: "34a35300-73c0-4ecc-8f11-4779b7dc1378") {
-              slates {
-                  description
-              }
+        with TestClient(app): # This context manager forces the FastAPI startup event to run, which we use to populate our slate lineup configuration
+            executed = self.client.execute(
+                '''
+                    query GetSlateLineup {
+                      getSlateLineup(slateLineupId: "aaf47c0f-1361-4c8c-a89f-fa45e1dc2978") {
+                        slates {
+                            description
+                        }
+                      }
+                    }
+                ''',
+                context_value={"user_id": "johnjacobjingleheimerschmidt"},
+                executor=AsyncioExecutor())
+            assert executed == {
+                'data': {
+                    'slates': [{
+                        'description': "Android is where it's at"
+                    }],
+                }
             }
-          }
-        ''', executor=AsyncioExecutor())
-        assert executed == {
-            'data': {
-                'slates': [{
-                    'description': "Android is where it's at"
-                }],
-            }
-        }
 
-    def populate_candidate_table(self):
-        pass
-        # self.candidate_table.put_item(Item={
-        #     'id': 'a187ffb4-5c6f-4079-bad9-92442e97bdd1',
-        #     "display_name": 'tech',
-        #     "page_type": 'topic_page',
-        #     "slug": 'tech',
-        #     "query": 'query',
-        #     "curator_label": 'technology',
-        #     "is_displayed": True,
-        #     "is_promoted": False
-        # })
-        # self.candidate_set_table.put_item(Item={})
+    def populate_candidate_sets_table(self):
+        self.candidate_set_table.put_item(Item={
+            'id': '303174fc-a9ff-4a51-984a-e09ce7120d18',
+            'candidates': [],
+            'version': '1'
+        })
+        self.candidate_set_table.put_item(Item={
+            'id': 'a8425a46-187a-4cdb-8157-5d2f308c52cd',
+            'candidates': [],
+            'version': '1'
+        })
+        self.candidate_set_table.put_item(Item={
+            'id': 'dacc55ea-db8d-4858-a51d-e1c78298337e',
+            'candidates': [],
+            'version': '1'
+        })
+        self.candidate_set_table.put_item(Item={
+            'id': '7ef90242-ff7a-44ac-8a32-53193e4a23eb',
+            'candidates': [],
+            'version': '1'
+        })
