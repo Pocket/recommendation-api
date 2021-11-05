b0b9355d6ce7463c8622efbb0a726bf1634e89cd
Mathijs Miermans <m.miermans@gmail.com>
2021-11-05 16:00:22 -0700

    Fix Uvicorn autoreload
    It previously failed, because it tried to reload on /opt

--- .docker/images/app/start-reload.sh
+++ .docker/images/app/start-reload.sh
@@ -38,4 +38,4 @@ else
 fi
 
 # Start Uvicorn with live reload
-exec uvicorn --reload --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
+exec uvicorn --reload --reload-dir /app --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
