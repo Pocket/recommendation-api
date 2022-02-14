e6385f427aebbeb7908c2f17a68b8a39fb5c018e
Chelsea Troy <chelsea.dommert@gmail.com>
2022-02-14 17:33:57 -0600

    The case of the missing import apparently: CHELVIN STRIKES AGAIN

--- app/graphql/ranked_corpus_items.py
+++ app/graphql/ranked_corpus_items.py
@@ -3,7 +3,7 @@ from graphene_pydantic import PydanticObjectType
 from app.models.ranked_corpus_items_instance import RankedCorpusItemsInstance
 from app.models.slate import SlateModel
 # This import needs to exist before Slate so that the below class can resolve the recommendation model
-from app.graphql.recommendation import Recommendation
+from app.graphql.corpus_item import CorpusItem
 
 
 class RankedCorpusItems(PydanticObjectType):
