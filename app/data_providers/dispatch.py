7544bf82e8a98bdabdf5521f172bf4ab74c0fca7
Chelsea Troy <chelsea.dommert@gmail.com>
2022-02-22 13:31:12 -0600

    Begin spiking this integration

--- app/data_providers/dispatch.py
+++ app/data_providers/dispatch.py
@@ -1,15 +1,12 @@
 import itertools
 import random
-import uuid
 from asyncio import gather
 
 from app.data_providers.curation_api_client import CurationAPIClient
 from app.data_providers.metrics_client import MetricsClient
 from app.data_providers.slate_provider import SlateProvider
-from app.models.corpus_item_model import CorpusItemModel
-from app.models.metrics.firefox_new_tab_metrics_factory import FirefoxNewTabMetricsFactory
 from app.models.ranked_corpus_items_instance import RankedCorpusItemsInstance
-from app.rankers.algorithms import firefox_thompson_sampling_1day
+from snowplow_tracker import Tracker, Emitter, Subject
 
 
 class Dispatch:
@@ -52,15 +49,15 @@ class Dispatch:
         # If no rankers, return the same list in the same order that the corpus api handed us
         ranked_items = flattened_unranked_items
 
-        # I do not check for duplicate rankers here. I know the old flow does.
-        # This error is not catastrophic (at most, it reorders items a few times)
-        # It's also not likely (an eng AND their reviewer would have to miss two identical lines next to each other)
-        # And it's not insidious (would be easy to find and verify if we suspected it was happening)
-        # So it's not worth complicating the code to check for IMO
         for ranker in experiment.rankers:
             ranker_kwargs = await self.metrics_client.get_engagement_metrics(ranked_items, ranker)
-
             ranked_items = ranker(ranked_items, **ranker_kwargs)
+
+        # log to snowplow
+        emitter = Emitter("some.uri.apparently")
+        subject = Subject.set_lang("en")
+        tracker = Tracker(emitter, subject=subject)
+
 
         return RankedCorpusItemsInstance(
             id=slate_id,
