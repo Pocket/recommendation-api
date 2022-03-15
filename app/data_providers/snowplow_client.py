cafa67cfcd35b8e6fbda82ab70695d9fce237aed
Chelsea Troy <chelsea.dommert@gmail.com>
2022-03-15 15:55:31 -0500

    Attempt to send a snowplow event with the exact format backend is using to confirm that the  thing works at all

--- app/data_providers/snowplow_client.py
+++ app/data_providers/snowplow_client.py
@@ -1,7 +1,7 @@
 import uuid
 from abc import ABC
 
-from snowplow_tracker import Emitter, Tracker, payload
+from snowplow_tracker import Emitter, Tracker, payload, SelfDescribingJson
 import os
 
 from app.graphql.corpus_item import CorpusItem
@@ -11,6 +11,7 @@ class SnowplowFetchable(ABC):
     async def log_event(self, user_id: int, items: [CorpusItem]) -> None:
         return NotImplemented
 
+
 class SnowplowClient(SnowplowFetchable):
     def __init__(self, tracker=None):
         emitter = Emitter(os.getenv("SNOWPLOW_URI"))
@@ -18,8 +19,9 @@ class SnowplowClient(SnowplowFetchable):
 
     async def log_event(
             self,
-            user_id: int = 0, # I am assigning zero to "the null value user id" because according to the spec this has to be an int
-            items: [CorpusItem]=[]
+            user_id: int = 0,
+            # I am assigning zero to "the null value user id" because according to the spec this has to be an int
+            items: [CorpusItem] = []
     ) -> None:
         tracking_payload = payload.Payload()
         tracking_payload.add_json(dict_={
@@ -38,4 +40,32 @@ class SnowplowClient(SnowplowFetchable):
             type_when_not_encoded="recs_api_recommendations"
         )
 
-        self.tracker.track(pb=tracking_payload)
+        self.tracker.track_self_describing_event(
+            event_json=SelfDescribingJson(
+                'iglu:com.pocket/scheduled_corpus_item/jsonschema/1-0-1',
+                {
+                    "object_version": 'new',
+                    "scheduled_corpus_item_external_id": '789-xyz',
+                    "scheduled_at": 1893456000,
+                    "url": 'thisisjonathanandchelseassweeturl.com',
+                    "approved_corpus_item_external_id": '123-abc',
+                    "scheduled_surface_id": 'NEW_TAB_EN_US',
+                    "created_at": 1647372554,
+                    "created_by": 'Chelsea',
+                    "updated_at": 1647372554,
+                    "updated_by": 'Chelsea',
+                    "scheduled_surface_name": 'New Tab (en-US)',
+                    "scheduled_surface_utc_offset": '-500'
+                }
+            ),
+            context=[SelfDescribingJson(
+                'iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0',
+                {
+                    'iglu:com.pocket/object_update/jsonschema/1-0-5',
+                    {
+                        "trigger": 'scheduled_corpus_item_removed',
+                        "object": 'scheduled_corpus_item'
+                    }
+                }
+            )]
+        )
