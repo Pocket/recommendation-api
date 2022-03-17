704971cd2e7bc816efdd31cb60f7c70b89eb035a
Chelsea Troy <chelsea.dommert@gmail.com>
2022-03-17 14:11:23 -0500

    This should send the expected snowplow events, with the exception of the fact that we don't know where tile_id is supposed to come from, how it's calculated, or what data type firefox requires for it

--- app/data_providers/snowplow_client.py
+++ app/data_providers/snowplow_client.py
@@ -14,58 +14,58 @@ class SnowplowFetchable(ABC):
 
 class SnowplowClient(SnowplowFetchable):
     def __init__(self, tracker=None):
-        emitter = Emitter(os.getenv("SNOWPLOW_URI"))
-        self.tracker = tracker if tracker else Tracker(emitter)
+        emitter = Emitter(endpoint=os.getenv("SNOWPLOW_URI_DEV"), protocol='https')
+        self.tracker = tracker if tracker else Tracker(emitter, app_id="recs-api")
 
     async def log_event(
             self,
-            user_id: int = 0,
+            slate_id: str,
             # I am assigning zero to "the null value user id" because according to the spec this has to be an int
+            user_id: int = 0,
             items: [CorpusItem] = []
     ) -> None:
-        tracking_payload = payload.Payload()
-        tracking_payload.add_json(dict_={
-            "recommendation_id": str(uuid.uuid4()),
+        self.tracker.track_self_describing_event(
+            context=[SelfDescribingJson(
+                'iglu:com.pocket/recommendation_result/jsonschema/1-0-0',
+                await self.recommendation_result_snowplow_dict(items, slate_id))],
+            event_json=SelfDescribingJson(
+                'iglu:com.pocket/object_update/jsonschema/1-0-6',
+                {
+                    "trigger": 'result_recommended',
+                    "object": 'recommendation_result'
+                }))
+
+        for corpus_item in items:
+            self.tracker.track_self_describing_event(
+                context=[SelfDescribingJson(
+                    'iglu:com.pocket/tile_recommendation_mapping/jsonschema/1-0-0',
+                    await self.tile_recommendation_mapping_dict(corpus_item, slate_id))],
+                event_json=SelfDescribingJson(
+                    'iglu:com.pocket/object_update/jsonschema/1-0-6',
+                    {
+                        "trigger": 'result_recommended',
+                        "object": 'tile_recommendation_mapping'
+                    }))
+        self.tracker.flush()
+
+    async def tile_recommendation_mapping_dict(self, corpus_item, slate_id):
+        return {
+            "tile_id": "Blocked on Info",
+            "scheduled_corpus_item_external_id": corpus_item.id,
+            "slate_id": slate_id,
             "slate_experiment_id": None,  # A null value for now
-            "user_id": user_id,
-            "items": [
+            "url": corpus_item.id  # For corpus items, the ID is a url
+        }
+
+    async def recommendation_result_snowplow_dict(self, items, slate_id):
+        return {
+            "recommendation_result_id": str(uuid.uuid4()),
+            "slate_id": slate_id,
+            "slate_experiment_id": None,  # A null value for now
+            "recommendation_result_items": [
                 {
                     "recommendation_item_id": str(uuid.uuid4()),
                     "scheduled_corpus_item_id": corpus_item.id,
                 } for corpus_item in items
-            ],
-        },
-            encode_base64=False,
-            type_when_encoded="encoded_recs_api_recommendations",
-            type_when_not_encoded="recs_api_recommendations"
-        )
-
-        self.tracker.track_self_describing_event(
-            event_json=SelfDescribingJson(
-                'iglu:com.pocket/scheduled_corpus_item/jsonschema/1-0-1',
-                {
-                    "object_version": 'new',
-                    "scheduled_corpus_item_external_id": '789-xyz',
-                    "scheduled_at": 1893456000,
-                    "url": 'thisisjonathanandchelseassweeturl.com',
-                    "approved_corpus_item_external_id": '123-abc',
-                    "scheduled_surface_id": 'NEW_TAB_EN_US',
-                    "created_at": 1647372554,
-                    "created_by": 'Chelsea',
-                    "updated_at": 1647372554,
-                    "updated_by": 'Chelsea',
-                    "scheduled_surface_name": 'New Tab (en-US)',
-                    "scheduled_surface_utc_offset": '-500'
-                }
-            ),
-            context=[SelfDescribingJson(
-                'iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0',
-                {
-                    'iglu:com.pocket/object_update/jsonschema/1-0-5',
-                    {
-                        "trigger": 'scheduled_corpus_item_removed',
-                        "object": 'scheduled_corpus_item'
-                    }
-                }
-            )]
-        )
+            ]
+        }
--- tests/unit/data_providers/test_snowplow_client.py
+++ tests/unit/data_providers/test_snowplow_client.py
@@ -7,10 +7,18 @@ from app.graphql.corpus_item import CorpusItem
 
 class MockTracker():
     def __init__(self):
-        self.most_recent_payload = {}
+        self.tracked_contexts = []
+        self.tracked_events = []
+        self.flush_called = False
 
-    def track(self, pb):
-        self.most_recent_payload = pb
+    def track_self_describing_event(self, context, event_json):
+        self.tracked_contexts.append(context)
+        self.tracked_events.append(event_json)
+        return self
+
+    def flush(self):
+        self.flush_called = True
+        return self
 
 @pytest.mark.asyncio
 async def test_log_event():
@@ -19,18 +27,33 @@ async def test_log_event():
     snowplow_tracking = SnowplowClient(tracker=mock_tracker)
 
     test_user_id = 14464
-    await snowplow_tracking.log_event(user_id=test_user_id, items = [CorpusItem(id='example-uuid-1'), CorpusItem(id='example-uuid-2'), CorpusItem(id='example-uuid-3')])
+    await snowplow_tracking.log_event(
+        user_id=test_user_id,
+        slate_id="uuid-representing-a-slate",
+        items = [CorpusItem(id='example-uuid-1'), CorpusItem(id='example-uuid-2'), CorpusItem(id='example-uuid-3')]
+    )
 
-    tracked_payload = json.loads(
-        mock_tracker.most_recent_payload.__dict__
-            .get('nv_pairs')
-            .get('recs_api_recommendations'))
-    assert tracked_payload.get('recommendation_id') is not None # This is a uuid generated at call time
-    assert tracked_payload.get('user_id') == test_user_id
-    assert tracked_payload.get('slate_experiment_id') is None
+    assert len(mock_tracker.tracked_contexts) == 4
+    assert mock_tracker.tracked_contexts[0][0].schema == "iglu:com.pocket/recommendation_result/jsonschema/1-0-0"
+    assert mock_tracker.tracked_contexts[0][0].data.get("slate_id") == "uuid-representing-a-slate"
+    assert [item.get("scheduled_corpus_item_id") for item in mock_tracker.tracked_contexts[0][0].data.get("recommendation_result_items")] == \
+           ['example-uuid-1', 'example-uuid-2', 'example-uuid-3']
 
-    item_list = tracked_payload.get('items')
-    assert len(item_list) == 3
-    assert all([item.get('recommendation_item_id') for item in item_list]) # These are uuids generated at call time
-    assert [item.get('scheduled_corpus_item_id') for item in item_list] == ["example-uuid-1", "example-uuid-2", "example-uuid-3"]
+    assert all([context[0].schema == "iglu:com.pocket/tile_recommendation_mapping/jsonschema/1-0-0" for context in mock_tracker.tracked_contexts[1:3]])
+    assert all([context[0].data.get('slate_id') == "uuid-representing-a-slate" for context in mock_tracker.tracked_contexts[1:3]])
+    
+    assert mock_tracker.tracked_contexts[1][0].data.get('scheduled_corpus_item_external_id') == "example-uuid-1"
+    assert mock_tracker.tracked_contexts[1][0].data.get('url') == "example-uuid-1"
+    assert mock_tracker.tracked_contexts[2][0].data.get('scheduled_corpus_item_external_id') == "example-uuid-2"
+    assert mock_tracker.tracked_contexts[2][0].data.get('url') == "example-uuid-2"
+    assert mock_tracker.tracked_contexts[3][0].data.get('scheduled_corpus_item_external_id') == "example-uuid-3"
+    assert mock_tracker.tracked_contexts[3][0].data.get('url') == "example-uuid-3"
+
+    assert len(mock_tracker.tracked_events) == 4
+    assert all([event.schema == "iglu:com.pocket/object_update/jsonschema/1-0-6" for event in mock_tracker.tracked_events])
+
+    mock_tracker.tracked_events[0].data.get('object') == "recommendation_result"
+    assert all([event.data.get('object') == "tile_recommendation_mapping" for event in mock_tracker.tracked_events[1:]])
+
+    assert mock_tracker.flush_called
 
