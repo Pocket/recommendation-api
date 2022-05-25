1076fa43e4250956579fcc737250792d8f85bfa9
Matthew Cooper <mcooper@getpocket.com>
2022-05-25 15:47:59 -0700

    initial user-impression filtering using feature store

--- app/models/slate_lineup_config.py
+++ app/models/slate_lineup_config.py
@@ -8,7 +8,7 @@ from app.json.utils import parse_to_dict
 from app.models.metrics.slate_metrics_factory import SlateMetricsFactory
 from app.models.slate_lineup_experiment import SlateLineupExperimentModel
 from app.models.slate_config import SlateConfigModel
-from app.rankers import get_ranker, PERSONALIZED_RANKERS, POCKET_THOMPSON_SAMPLING_RANKERS
+from app.rankers import get_ranker, PERSONALIZED_TOPIC_RANKERS, POCKET_THOMPSON_SAMPLING_RANKERS
 from app.models.personalized_topic_list import PersonalizedTopicList
 
 
@@ -127,7 +127,7 @@ class SlateLineupConfigModel:
                         slate_lineup_id,
                         [s.id for s in slate_configs])
                 }
-            elif ranker in PERSONALIZED_RANKERS:
+            elif ranker in PERSONALIZED_TOPIC_RANKERS:
                 ranker_kwargs = {
                     "personalized_topics": await PersonalizedTopicList.get(user_id)
                 }
