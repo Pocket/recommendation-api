758e6929cf0e1d7bd5ec2a042ee49fc763aa1b2a
Mathijs Miermans <m.miermans@gmail.com>
2022-06-30 09:39:36 -0700

    Cleanup PocketGraphClientSession

--- app/data_providers/PocketGraphClientSession.py
+++ app/data_providers/PocketGraphClientSession.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 from aiohttp import ClientSession
 
-from app.config import ENV, ENV_PROD
+from app.config import ENV, ENV_PROD, VERSION
 
 
 @dataclass
@@ -11,20 +11,28 @@ class PocketGraphConfig:
     DEV_ENDPOINT_URL = 'https://client-api.getpocket.dev'
     ENDPOINT_URL = PROD_ENDPOINT_URL if ENV == ENV_PROD else DEV_ENDPOINT_URL
 
-    APOLLO_CLIENT_NAME = 'recommendations-api'
-    APOLLO_CLIENT_VERSION = 1
+    CLIENT_NAME = 'recommendations-api'
+    CLIENT_VERSION = VERSION
 
 
 class PocketGraphClientSession(ClientSession):
     """
-    This class subclasses ClientSession and initializes it for connecting to Pocket's Apollo Federated GraphQL server.
+    This is an aiohttp.ClientSession initialized for connecting to Pocket's Apollo Federated GraphQL server.
+
+    Example usage:
+        ```python
+        session = PocketGraphClientSession(config=PocketGraphConfig())
+        query = 'query Collections { getCollections { collections { title } } }'
+        async with session.post(url='/', json={'query': query}) as resp:
+            response_json = await resp.json()
+        ```
     """
 
     def __init__(self, config: PocketGraphConfig) -> None:
         super().__init__(
             base_url=config.ENDPOINT_URL,
             headers={
-                'apollographql-client-name': config.APOLLO_CLIENT_NAME,
-                'apollographql-client-version': str(config.APOLLO_CLIENT_VERSION),
+                'apollographql-client-name': config.CLIENT_NAME,
+                'apollographql-client-version': config.CLIENT_VERSION,
             }
         )
