11869e70190d1cd3d2915387e7995ac36205a630
Mathijs Miermans <mmiermans@users.noreply.github.com>
2022-07-13 15:15:23 -0700

    Pass hashed user id and guid into Snowplow user entity (#934)
    * Pass hashed user id and guid into Snowplow user entity
    
    * Fix Snowplow validation error for user null values

--- app/graphql/user_middleware.py
+++ app/graphql/user_middleware.py
@@ -1,3 +1,6 @@
+from app.models.user import User
+
+
 class UserMiddleware(object):
     """
     This piece of middleware adds the pocket user id to the graphql resolver context
@@ -12,13 +15,21 @@ class UserMiddleware(object):
         since this application lives within Pocket's VPC we are trusting that the upstream caller's
         have validated the JWT token and only set the userId in the header if it was valid
 
+        List of all headers that are passed to services that sit behind the Pocket Graph:
+        @see https://github.com/Pocket/client-api/blob/main/api-docs/docs/headers.md
+
         Should we also want to do our own validation, the full JWT token also exists as 'token' in the header
         https://github.com/Pocket/client-api/blob/main/src/main.ts#L18-L26
         """
-        try:
-            info.context['user_id'] = info.context.get('request').headers.get('userId')
-        except AttributeError:
-            """No user id so let's explicitly set it to none"""
-            info.context['user_id'] = None
+        headers = info.context.get('request').headers
+
+        info.context['user_id'] = headers.get('userId')
+
+        info.context['user'] = User(
+            user_id=headers.get('userId'),
+            hashed_user_id=headers.get('encodedId'),
+            hashed_guid=headers.get('encodedGuid'),
+            guid=headers.get('guid'),
+        )
 
         return next(root, info, **args)
