6e2ec5379ce956a24e1d932945606f3c9078729c
Mathijs Miermans <m.miermans@gmail.com>
2022-08-15 19:31:17 -0700

    Fix test_user_reference_resolver

--- tests/functional/graphql/test_user_reference_resolver.py
+++ tests/functional/graphql/test_user_reference_resolver.py
@@ -1,67 +1,76 @@
-from graphql.execution.executors.asyncio import AsyncioExecutor
-from graphene.test import Client
+from datetime import datetime
+
 from fastapi.testclient import TestClient
 
-from app.data_providers.user_recommendation_preferences_provider import (
-    UserRecommendationPreferencesProviderV2,
-)
-from app.graphql.graphql_router import schema
+from app.data_providers.user_recommendation_preferences_provider import UserRecommendationPreferencesProviderV2
 from app.main import app
 from app.models.user_ids import UserIds
-from tests.assets.topics import populate_topics
+from app.models.user_recommendation_preferences import UserRecommendationPreferencesModelV2
+from tests.assets.topics import populate_topics, business_topic
 from tests.functional.test_dynamodb_base import TestDynamoDBBase
 
+
 from unittest.mock import patch
-from collections import namedtuple
-
-MockResponse = namedtuple('MockResponse', 'status')
 
 
-class TestUpdateUserRecommendationPreferences(TestDynamoDBBase):
-    client: Client
+mock_user_preferences = UserRecommendationPreferencesModelV2(
+    hashed_user_id='1-hashed',
+    updated_at=datetime(2022, 7, 14, 8, 30),
+    preferred_topics=[business_topic],
+)
+
+
+class TestUserReferenceResolver(TestDynamoDBBase):
 
     async def asyncSetUp(self):
         await super().asyncSetUp()
         populate_topics(self.metadata_table)
-        self.client = Client(schema)
         self.user_ids = UserIds(
             user_id=1,
             hashed_user_id='1-hashed',
         )
 
-    @patch.object(UserRecommendationPreferencesProviderV2, 'fetch', return_value=None)
-    def test_update_user_recommendation_preferences(self, mock_data_provider_fetch_v2):
-        with TestClient(app):
-            executed = self.client.execute(
-                '''
-                query ($representations: [_Any!]!) {
-                  _entities(representations: $representations) {
-                    ... on User {
-                      id
-                      recommendationPreferences {
-                        preferredTopics {
-                          name
-                          id
+    @patch.object(UserRecommendationPreferencesProviderV2, 'fetch', return_value=mock_user_preferences)
+    def test_user_reference_resolver(self, mock_data_provider_fetch_v2):
+        # Note: Unlike other functional tests, using graphene.test.Client causes the test below to get stuck (timeout).
+        #       Making the request directly using the TestClient does work.
+        with TestClient(app) as client:
+            response = client.post(
+                "/",
+                json={
+                    'query': '''
+                        query ($representations: [_Any!]!) {
+                          _entities(representations: $representations) {
+                            ... on User {
+                              id
+                              recommendationPreferences {
+                                preferredTopics {
+                                  name
+                                  id
+                                }
+                              }
+                            }
+                          }
                         }
-                      }
-                    }
-                  }
-                }
-                ''',
-                variables={
-                    'representations': [
-                        {
-                            '__typename': 'User',
-                            'id': self.user_ids.hashed_user_id,
-                        },
-                    ],
+                    ''',
+                    'variables': {
+                        'representations': [
+                            {
+                                '__typename': 'User',
+                                'id': self.user_ids.hashed_user_id,
+                            },
+                        ],
+                    },
                 },
-                executor=AsyncioExecutor())
+            )
+
+            executed = response.json()
 
             assert not executed.get('error')
             entities = executed['data']['_entities']
 
             assert len(entities) == 1
             assert entities[0]['id'] == self.user_ids.hashed_user_id
-            assert entities[0]['recommendationPreferences']['preferredTopics'] == []
-            print("Done")
+            assert entities[0]['recommendationPreferences']['preferredTopics'] == [
+                {'id': business_topic.id, 'name': business_topic.name}
+            ]
