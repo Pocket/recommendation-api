b454ab41317c2afe64b4b923e6c6c28b41996e00
Mathijs Miermans <m.miermans@gmail.com>
2022-08-24 21:09:40 -0700

    Fix Cannot query field 'item_id'

--- app/graphql/objects/legacy/item.py
+++ app/graphql/objects/legacy/item.py
@@ -3,7 +3,7 @@ import strawberry
 from app.models.item import ItemModel
 
 
-@strawberry.federation.type(keys=["item_id"])
+@strawberry.federation.type(keys=["itemId"])
 @strawberry.experimental.pydantic.type(model=ItemModel)
 class Item:
     item_id: str  # This type is a 'str' and not an 'ID' in our graph.
