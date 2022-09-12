cf4a96ce2e840ce629b0abfe47966f99853dfd8b
Mathijs Miermans <m.miermans@gmail.com>
2022-09-12 15:36:32 -0700

    Add CollectionSlateProvider

---
+++ app/data_providers/slate_providers/collection_slate_provider.py
@@ -0,0 +1,21 @@
+from app.data_providers.corpus.corpus_feature_group_client import CorpusFeatureGroupClient
+from app.models.corpus_recommendation_model import CorpusRecommendationModel
+from app.models.corpus_slate_model import CorpusSlateModel
+from app.models.topic import TopicModel
+
+
+class CollectionSlateProvider:
+
+    _CANDIDATE_SET = '92af3dae-25c9-46c3-bf05-18082aacc7e1'
+
+    def __init__(self, corpus_feature_group_client: CorpusFeatureGroupClient):
+        self.corpus_feature_group_client = corpus_feature_group_client
+
+    async def get_slate(self) -> CorpusSlateModel:
+        items = await self.corpus_feature_group_client.fetch(self._CANDIDATE_SET)
+
+        return CorpusSlateModel(
+            headline='Our most-read Collections',
+            subheadline='Dive into our editors’ curated lists of stories around a unique subject',
+            recommendations=[CorpusRecommendationModel(corpus_item=item) for item in items],
+        )
