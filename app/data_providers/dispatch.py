baeec4b8e2e896a5047b538e84a4557941e24d4a
Mathijs Miermans <mmiermans@users.noreply.github.com>
2022-09-13 09:58:27 -0700

    Remove unused Fx New Tab and middleware code (#950)
    * Remove unused Fx New Tab code
    
    * Delete unused User middleware
    
    * Remove Sentry middleware

--- app/data_providers/dispatch.py
+++ app/data_providers/dispatch.py
@@ -5,10 +5,6 @@ import logging
 import uuid
 
 from app.data_providers.corpus.corpus_feature_group_client import CorpusFeatureGroupClient
-from app.data_providers.corpus.corpus_fetchable import CorpusFetchable
-from app.data_providers.snowplow.snowplow_corpus_slate_tracker import SnowplowCorpusSlateTracker
-from app.data_providers.metrics_client import MetricsFetchable
-from app.data_providers.slate_provider import SlateProvider
 from app.data_providers.topic_slate_provider import TopicSlateProvider
 from app.data_providers.topic_provider import TopicProvider
 from app.data_providers.user_recommendation_preferences_provider import UserRecommendationPreferencesProvider
@@ -118,50 +114,4 @@ class HomeDispatch:
             corpus_client=self.corpus_client,
             user_recommendation_preferences_provider=self.user_recommendation_preferences_provider,
             topic_provider=self.topic_provider,
-        )
-
-
-class RankingDispatch:
-    """
-    This class is responsible for accepting:
-
-     a dependency to get items to rank (api_client),
-     a strategy for ranking them (slate_provider),
-     and the data to execute ranking (metrics_client),
-
-     and then using those three things to shape the list of items to the order and size we serve to a client.
-
-     If there are NO rankers, it sends the original list of items, with no reshaping, to the client.
-    """
-    def __init__(
-            self,
-            corpus_client: CorpusFetchable,
-            slate_provider: SlateProvider,
-            metrics_client: MetricsFetchable
-    ):
-        self.corpus_client = corpus_client
-        self.slate_provider = slate_provider
-        self.metrics_client = metrics_client
-
-    async def get_ranked_corpus_slate(self, slate_id) -> CorpusSlateModel:
-        """
-        From a slate identifier find the appropriate experiment. Then fetch the candidate set and sort the corpus items.
-        :param slate_id: defined in `slate_configs.json`
-        :return: CorpusSlateModel
-        """
-        corpus_slate_schema = self.slate_provider.get_slate(slate_id)
-        experiment = self.slate_provider.get_random_experiment(slate_id)
-
-        # Fetch Corporeal Candidates
-        items = await self.corpus_client.get_corpus_items(experiment.corpus_ids())
-        items = await self.metrics_client.rank_items(items, experiment.rankers)
-
-        recommendations = [CorpusRecommendationModel(id=uuid.uuid4().hex, corpus_item=item) for item in items]
-
-        return CorpusSlateModel(
-            id=slate_id,
-            recommended_at=datetime.now(tz=timezone.utc),
-            headline=corpus_slate_schema.displayName,
-            subheadline=corpus_slate_schema.description,
-            recommendations=recommendations,
         )
