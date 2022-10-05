e34fa0a72eb663eae68c64229557d89e64283ddb
Mathijs Miermans <m.miermans@gmail.com>
2022-10-05 16:18:50 -0700

    chore(Home): Refactor RecommendedReadsSlateProvider based on SlateProvider

--- app/data_providers/slate_providers/recommended_reads_slate_provider.py
+++ app/data_providers/slate_providers/recommended_reads_slate_provider.py
@@ -1,20 +1,13 @@
-from app.data_providers.corpus.corpus_feature_group_client import CorpusFeatureGroupClient
-from app.models.corpus_recommendation_model import CorpusRecommendationModel
-from app.models.corpus_slate_model import CorpusSlateModel
-
-
 class RecommendedReadsSlateProvider:
 
-    _CANDIDATE_SET_ID = '5f0dae93-a5a8-439a-a2e2-5d418c04bc98'
+    @property
+    def candidate_set_id(self) -> str:
+        return '5f0dae93-a5a8-439a-a2e2-5d418c04bc98'
 
-    def __init__(self, corpus_feature_group_client: CorpusFeatureGroupClient):
-        self.corpus_feature_group_client = corpus_feature_group_client
+    @property
+    def headline(self) -> str:
+        return 'Recommended Reads'
 
-    async def get_slate(self) -> CorpusSlateModel:
-        items = await self.corpus_feature_group_client.fetch(self._CANDIDATE_SET_ID)
-
-        return CorpusSlateModel(
-            headline='Recommended Reads',
-            subheadline='Curated by Pocket',
-            recommendations=[CorpusRecommendationModel(corpus_item=item) for item in items],
-        )
+    @property
+    def subheadline(self) -> str:
+        return 'Curated by Pocket'
