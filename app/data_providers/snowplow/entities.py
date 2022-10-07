bc92298d4e963ff24216a7ef9dda43bc6b749705
Mathijs Miermans <m.miermans@gmail.com>
2022-10-06 18:38:59 -0700

    chore(Home): Deduplicate more Snowplow code

--- app/data_providers/snowplow/entities.py
+++ app/data_providers/snowplow/entities.py
@@ -1,38 +1,68 @@
-from typing import Any, Dict
+from aio_snowplow_tracker import SelfDescribingJson, Subject
 
 from app.models.corpus_slate_lineup_model import CorpusSlateLineupModel
 from app.models.corpus_slate_model import CorpusSlateModel
+from app.models.user_ids import UserIds
 
 
-def get_corpus_slate_entity(corpus_slate: CorpusSlateModel) -> Dict[str, Any]:
+def get_object_update_event(schema: str, object: str, trigger: str) -> SelfDescribingJson:
     """
+    :param schema: Versioned Snowplow schema URI
+    :param object: Describes _what_ is being updated. `object_update` contains an enum with allows values.
+    :param trigger: Describes _why_ the object is updated. String is not validated. The convention is to use snake case.
+    :return: Can be passed to 'event_json' of track_self_describing_event to emit an object_update event.
+    """
+    return SelfDescribingJson(schema=schema, data={'object': object, 'trigger': trigger})
+
+
+def get_user_entity(schema: str, user_ids: UserIds) -> SelfDescribingJson:
+    """
+    :param schema: Versioned Snowplow schema URI
+    :param user_ids:
+    :return: Snowplow user entity
+    """
+    user_entity = {k: v for k, v in user_ids.dict().items() if v is not None}
+
+    return SelfDescribingJson(schema=schema, data=user_entity)
+
+
+def get_corpus_slate_entity(schema: str, corpus_slate: CorpusSlateModel) -> SelfDescribingJson:
+    """
+    :param schema: Versioned Snowplow schema URI
     :param corpus_slate:
     :return: Snowplow corpus_slate entity
     """
-    return {
-        'corpus_slate_id': corpus_slate.id,
-        'recommended_at': int(corpus_slate.recommended_at.timestamp()),
-        'corpus_slate_configuration_id': corpus_slate.configuration_id,
-        'recommendations': [
-            {
-                'corpus_recommendation_id': recommendation.id,
-                'corpus_item': {
-                    'corpus_item_id': recommendation.corpus_item.id,
+    return SelfDescribingJson(
+        schema=schema,
+        data={
+            'corpus_slate_id': corpus_slate.id,
+            'recommended_at': int(corpus_slate.recommended_at.timestamp()),
+            'corpus_slate_configuration_id': corpus_slate.configuration_id,
+            'recommendations': [
+                {
+                    'corpus_recommendation_id': recommendation.id,
+                    'corpus_item': {
+                        'corpus_item_id': recommendation.corpus_item.id,
+                    }
                 }
-            }
-            for recommendation in corpus_slate.recommendations
-        ]
-    }
+                for recommendation in corpus_slate.recommendations
+            ]
+        },
+    )
 
 
-def get_corpus_slate_lineup_entity(corpus_slate_lineup: CorpusSlateLineupModel) -> Dict[str, Any]:
+def get_corpus_slate_lineup_entity(schema: str, corpus_slate_lineup: CorpusSlateLineupModel) -> SelfDescribingJson:
     """
+    :param schema: Versioned Snowplow schema URI
     :param corpus_slate_lineup:
     :return: Snowplow corpus_slate_lineup entity
     """
-    return {
-        'corpus_slate_lineup_id': corpus_slate_lineup.id,
-        'recommended_at': int(corpus_slate_lineup.recommended_at.timestamp()),
-        'recommendation_surface_id': corpus_slate_lineup.recommendation_surface_id.value,
-        'slates': [get_corpus_slate_entity(corpus_slate) for corpus_slate in corpus_slate_lineup.slates]
-    }
+    return SelfDescribingJson(
+        schema=schema,
+        data={
+            'corpus_slate_lineup_id': corpus_slate_lineup.id,
+            'recommended_at': int(corpus_slate_lineup.recommended_at.timestamp()),
+            'recommendation_surface_id': corpus_slate_lineup.recommendation_surface_id.value,
+            'slates': [get_corpus_slate_entity(corpus_slate) for corpus_slate in corpus_slate_lineup.slates]
+        },
+    )
--- app/data_providers/snowplow/snowplow_corpus_slate_lineup_tracker.py
+++ app/data_providers/snowplow/snowplow_corpus_slate_lineup_tracker.py
@@ -1,24 +1,28 @@
+from aio_snowplow_tracker import Tracker
 from aws_xray_sdk.core import xray_recorder
-from aio_snowplow_tracker import Tracker, Subject, SelfDescribingJson
 
 from app.data_providers.snowplow.config import SnowplowConfig
-from app.data_providers.snowplow.entities import get_corpus_slate_entity, get_corpus_slate_lineup_entity
+from app.data_providers.snowplow.entities import (
+    get_corpus_slate_lineup_entity,
+    get_object_update_event,
+    get_user_entity,
+)
+from app.data_providers.snowplow.subject import get_subject
 from app.models.corpus_slate_lineup_model import CorpusSlateLineupModel
-from app.models.corpus_slate_model import CorpusSlateModel
 from app.models.user_ids import UserIds
 
 
 class SnowplowCorpusSlateLineupTracker:
     """
-    Implements tracking the recommendation of a CorpusSlate in SnowPlow, with the goal of tracking any metadata for a
-    CorpusRecommendation that we are interested in. Clients will only emit `CorpusRecommendation.id` in events.
+    Implements tracking the recommendation of a CorpusSlateLineup in SnowPlow, with the goal of tracking any metadata
+    for a CorpusRecommendation that we are interested in. Clients will only emit `CorpusRecommendation.id` in events.
     """
 
     def __init__(self, tracker: Tracker, snowplow_config: SnowplowConfig):
         self.tracker = tracker
         self.snowplow_config = snowplow_config
 
-    @xray_recorder.capture_async('data_providers.SnowplowCorpusSlateTracker.track')
+    @xray_recorder.capture_async('data_providers.SnowplowCorpusSlateLineupTracker.track')
     async def track(self, corpus_slate_lineup: CorpusSlateLineupModel, user: UserIds):
         """
         Track the recommendation of a CorpusSlateLineup in Snowplow.
@@ -26,36 +30,14 @@ class SnowplowCorpusSlateLineupTracker:
         :param user: The user that the slate was recommended to.
         """
         await self.tracker.track_self_describing_event(
-            event_json=self._get_object_update_event(
+            event_json=get_object_update_event(
+                self.snowplow_config.OBJECT_UPDATE_SCHEMA,
                 object='corpus_slate_lineup',
                 trigger='corpus_slate_lineup_recommendation'
             ),
-            event_subject=self._get_subject(user),
+            event_subject=get_subject(user),
             context=[
-                self._get_corpus_slate_lineup_entity(corpus_slate_lineup),
-                self._get_user_entity(user),
+                get_corpus_slate_lineup_entity(self.snowplow_config.CORPUS_SLATE_SCHEMA, corpus_slate_lineup),
+                get_user_entity(self.snowplow_config.USER_SCHEMA, user),
             ],
-        )
-
-    def _get_subject(self, user: UserIds):
-        return Subject().set_user_id(str(user.user_id))
-
-    def _get_object_update_event(self, object: str, trigger: str) -> SelfDescribingJson:
-        return SelfDescribingJson(
-            schema=self.snowplow_config.OBJECT_UPDATE_SCHEMA,
-            data={'object': object, 'trigger': trigger}
-        )
-
-    def _get_corpus_slate_lineup_entity(self, corpus_slate_lineup: CorpusSlateLineupModel) -> SelfDescribingJson:
-        return SelfDescribingJson(
-            schema=self.snowplow_config.CORPUS_SLATE_LINEUP_SCHEMA,
-            data=get_corpus_slate_lineup_entity(corpus_slate_lineup),
-        )
-
-    def _get_user_entity(self, user: UserIds) -> SelfDescribingJson:
-        user_entity = {k: v for k, v in user.dict().items() if v is not None}
-
-        return SelfDescribingJson(
-            schema=self.snowplow_config.USER_SCHEMA,
-            data=user_entity,
         )
