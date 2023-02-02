2b0356d628c051c389f672081cff57347625e799
Evgeny Pavlov <epavlov@mozilla.com>
2023-02-02 11:38:26 -0800

    Filter original article from right rail recs (#1004)

--- app/graphql/syndicated_article.py
+++ app/graphql/syndicated_article.py
@@ -6,10 +6,11 @@ from app.graphql.corpus_recommendation import CorpusRecommendation
 from app.graphql.resolvers.item2item_resolvers import resolve_syndicated_end_of_article, resolve_syndicated_right_rail
 
 
-@strawberry.federation.type(keys=["itemId publisherUrl"])
+@strawberry.federation.type(keys=["itemId publisherUrl originalItemId"])
 class SyndicatedArticle:
     itemId: strawberry.ID  # this corresponds to Snowflake resolved_id
     publisherUrl: str
+    originalItemId: strawberry.ID
 
     relatedEndOfArticle: List[CorpusRecommendation] = strawberry.field(
         resolver=resolve_syndicated_end_of_article,
@@ -19,5 +20,10 @@ class SyndicatedArticle:
         description='Recommend similar articles from the same publisher.')
 
     @classmethod
-    def resolve_reference(cls, itemId: strawberry.ID, publisherUrl: str):
-        return SyndicatedArticle(itemId=itemId, publisherUrl=publisherUrl)
+    def resolve_reference(cls,
+                          itemId: strawberry.ID,
+                          publisherUrl: str,
+                          originalItemId: strawberry.ID):
+        return SyndicatedArticle(itemId=itemId,
+                                 publisherUrl=publisherUrl,
+                                 originalItemId=originalItemId)
