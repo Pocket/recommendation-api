8e92ddf0b862bb370b84bd73379d0047244f571b
evgeny pavlov <epavlov@mozilla.com>
2023-02-13 11:46:44 -0800

    Fix directives in syndicated article

--- app/graphql/syndicated_article.py
+++ app/graphql/syndicated_article.py
@@ -1,32 +1,29 @@
 from typing import List
 
 import strawberry
+from strawberry.federation.schema_directives import Requires
 
 from app.graphql.corpus_recommendation import CorpusRecommendation
 from app.graphql.directives import CacheControl
 from app.graphql.resolvers.item2item_resolvers import resolve_syndicated_end_of_article, resolve_syndicated_right_rail
 
 
-@strawberry.federation.type(keys=["itemId publisherUrl originalItemId"])
+@strawberry.federation.type(keys=["itemId"])
 class SyndicatedArticle:
     itemId: strawberry.ID  # this corresponds to Snowflake resolved_id
-    publisherUrl: str
-    originalItemId: strawberry.ID
+
+    # these fields are from a different subgraph
+    # see https://www.apollographql.com/docs/federation/entities-advanced/#contributing-computed-entity-fields
+    publisherUrl: str = strawberry.federation.field(external=True)
+    originalItemId: strawberry.ID = strawberry.federation.field(external=True)
 
     relatedEndOfArticle: List[CorpusRecommendation] = strawberry.field(
         directives=[CacheControl(maxAge=3600)],
         resolver=resolve_syndicated_end_of_article,
         description='Recommend similar syndicated articles.')
     relatedRightRail: List[CorpusRecommendation] = strawberry.field(
-        directives=[CacheControl(maxAge=3600)],
+        directives=[CacheControl(maxAge=3600),
+                    Requires(fields='publisherUrl originalItemId')],
         resolver=resolve_syndicated_right_rail,
         description='Recommend similar articles from the same publisher.')
 
-    @classmethod
-    def resolve_reference(cls,
-                          itemId: strawberry.ID,
-                          publisherUrl: str,
-                          originalItemId: strawberry.ID):
-        return SyndicatedArticle(itemId=itemId,
-                                 publisherUrl=publisherUrl,
-                                 originalItemId=originalItemId)
