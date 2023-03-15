407405c59977cd0aa9681c02c4d71a2a041932c5
Mathijs Miermans <m.miermans@gmail.com>
2023-03-15 08:14:06 -0700

    chore(NewTab): Expand tileId deprecation message

--- app/graphql/corpus_recommendation.py
+++ app/graphql/corpus_recommendation.py
@@ -9,6 +9,8 @@ from app.models.corpus_recommendation_model import CorpusRecommendationModel
 class CorpusRecommendation:
     id: strawberry.ID
     tile_id: strawberry.auto = strawberry.field(
-        deprecation_reason='Only to be used by Firefox. Other clients should use `id`.')
+        deprecation_reason='Only to be used by Firefox. Other clients should use `id`. We plan to also migrate Firefox '
+                           'New Tab to use CorpusRecommendation.id instead of tileId to track recommendation telemetry.'
+    )
     corpus_item: strawberry.auto
     reason: strawberry.auto
