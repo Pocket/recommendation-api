bf8249894f63dac0e47eb3d291a01d41b7fb6c1d
Mathijs Miermans <m.miermans@gmail.com>
2023-04-18 10:41:22 -0700

    WIP

--- app/data_providers/slate_providers/new_tab_slate_provider.py
+++ app/data_providers/slate_providers/new_tab_slate_provider.py
@@ -1,7 +1,12 @@
 from typing import List
 
+from app.data_providers.PocketGraphClientSession import PocketGraphClientSession
+from app.data_providers.corpus.corpus_feature_group_client import CorpusFeatureGroupClient
+from app.data_providers.feature_group.corpus_engagement_provider import CorpusEngagementProvider
 from app.data_providers.slate_providers.slate_provider import SlateProvider
+from app.data_providers.translation import TranslationProvider
 from app.models.corpus_item_model import CorpusItemModel
+from app.models.corpus_slate_lineup_model import RecommendationSurfaceId
 from app.models.localemodel import LocaleModel
 from app.data_providers.util import integer_hash
 from app.models.corpus_recommendation_model import CorpusRecommendationModel
@@ -17,6 +22,24 @@ MIN_TILE_ID = 100 * 100000
 
 class NewTabSlateProvider(SlateProvider):
 
+    def __init__(
+            self,
+            pocket_graph_client_session: PocketGraphClientSession,
+            corpus_feature_group_client: CorpusFeatureGroupClient,
+            corpus_engagement_provider: CorpusEngagementProvider,
+            recommendation_surface_id: RecommendationSurfaceId,
+            locale: LocaleModel,
+            translation_provider: TranslationProvider):
+
+        super().__init__(
+            corpus_feature_group_client=corpus_feature_group_client,
+            corpus_engagement_provider=corpus_engagement_provider,
+            recommendation_surface_id=recommendation_surface_id,
+            locale=locale,
+            translation_provider=translation_provider)
+
+        self.pocket_graph_client_session = pocket_graph_client_session
+
     @property
     def candidate_set_id(self) -> str:
         # TODO: Query the candidate from the GraphQL scheduledSurface.items object. [DIS-452]
@@ -26,6 +49,49 @@ class NewTabSlateProvider(SlateProvider):
             return '92013292-bc4b-4ee1-815a-0e51c5953ff2'
         else:
             raise ValueError(f'Unexpected locale {self.locale} for {self.provider_name}')
+
+    async def get_candidate_corpus_items(self) -> List[CorpusItemModel]:
+        """
+        :return: The CorpusItems from the candidate set, without any rankers or filters applied.
+        """
+        """
+        Get all Unleash assignments for the given user/session.
+        :param user:
+        :return:
+        """
+        query = """
+            query UnleashAssignments($context: UnleashContext!) {
+              unleashAssignments(context: $context) {
+                assignments {
+                  assigned
+                  name
+                  payload
+                  variant
+                }
+              }
+            }
+            """
+
+        body = {
+            'query': query,
+            'variables': {
+                # @see https://studio.apollographql.com/graph/pocket-client-api/schema/reference/inputs/UnleashContext
+                'context': {
+                    'appName': self.unleash_config.APP_NAME,
+                    'environment': self.unleash_config.ENVIRONMENT,
+                    'userId': user.hashed_user_id,
+                    'sessionId': user.hashed_guid,
+                    'properties': {
+                        'locale': user.locale,
+                    }
+                }
+            }
+        }
+
+        async with self.pocket_graph_client_session.post(url='/', json=body, raise_for_status=True) as resp:
+            response_json = await resp.json()
+            assignments_data = response_json['data']['unleashAssignments']['assignments']
+            return [UnleashAssignmentModel.parse_obj(assignment) for assignment in assignments_data]
 
     @property
     def headline(self) -> str:
