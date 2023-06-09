7eea60ddb815b926828c7b96ada23286685ea2ad
Mathijs Miermans <m.miermans@gmail.com>
2023-06-09 07:26:51 -0700

    chore: Extend test cases based on documentation

--- tests/unit/data_providers/test_new_tab_dispatch.py
+++ tests/unit/data_providers/test_new_tab_dispatch.py
@@ -7,16 +7,40 @@ from app.models.corpus_slate_lineup_model import RecommendationSurfaceId
 @pytest.mark.parametrize(
     "locale,region,recommendation_surface_id",
     [
-        # Locale might be a language only
-        ('en', 'US', RecommendationSurfaceId.NEW_TAB_EN_US),
+        # Test cases below are from the Newtab locales/region documentation maintained by the Firefox integration team:
+        # https://docs.google.com/document/d/1omclr-eETJ7zAWTMI7mvvsc3_-ns2Iiho4jPEfrmZfo/edit
+        ('en-CA', 'US', RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('en-GB', 'US', RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('en-US', 'US', RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('en-CA', 'CA', RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('en-GB', 'CA', RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('en-US', 'CA', RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('de', 'DE', RecommendationSurfaceId.NEW_TAB_DE_DE),
+        ('de-AT', 'DE', RecommendationSurfaceId.NEW_TAB_DE_DE),
+        ('de-CH', 'DE', RecommendationSurfaceId.NEW_TAB_DE_DE),
+        ('en-CA', 'GB', RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('en-GB', 'GB', RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('en-US', 'GB', RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('en-CA', 'IE', RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('en-GB', 'IE', RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('en-US', 'IE', RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('fr-FR', 'FR', RecommendationSurfaceId.NEW_TAB_FR_FR),
+        ('it', 'IT', RecommendationSurfaceId.NEW_TAB_IT_IT),
+        ('es-ES', 'ES', RecommendationSurfaceId.NEW_TAB_ES_ES),
+        ('en-CA', 'IN', RecommendationSurfaceId.NEW_TAB_EN_INTL),
+        ('en-GB', 'IN', RecommendationSurfaceId.NEW_TAB_EN_INTL),
+        ('en-US', 'IN', RecommendationSurfaceId.NEW_TAB_EN_INTL),
+        ('de', 'CH', RecommendationSurfaceId.NEW_TAB_DE_DE),
+        ('de', 'AT', RecommendationSurfaceId.NEW_TAB_DE_DE),
+        ('de', 'BE', RecommendationSurfaceId.NEW_TAB_DE_DE),
+
+        # Locale can be a main language only.
+        ('en', 'CA', RecommendationSurfaceId.NEW_TAB_EN_US),
         ('en', 'GB', RecommendationSurfaceId.NEW_TAB_EN_GB),
         ('en', 'IE', RecommendationSurfaceId.NEW_TAB_EN_GB),
         ('en', 'IN', RecommendationSurfaceId.NEW_TAB_EN_INTL),
-        ('de', 'DE', RecommendationSurfaceId.NEW_TAB_DE_DE),
-        ('de', 'DE', RecommendationSurfaceId.NEW_TAB_DE_DE),
-        ('de', 'AT', RecommendationSurfaceId.NEW_TAB_DE_DE),
-        ('de', 'CH', RecommendationSurfaceId.NEW_TAB_DE_DE),
-        ('es', 'LI', RecommendationSurfaceId.NEW_TAB_ES_ES),
+        ('en', 'US', RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('es', 'ES', RecommendationSurfaceId.NEW_TAB_ES_ES),
         ('fr', 'FR', RecommendationSurfaceId.NEW_TAB_FR_FR),
         ('it', 'IT', RecommendationSurfaceId.NEW_TAB_IT_IT),
 
@@ -31,22 +55,17 @@ from app.models.corpus_slate_lineup_model import RecommendationSurfaceId
         ('en-GB', None, RecommendationSurfaceId.NEW_TAB_EN_GB),
         ('en-IE', None, RecommendationSurfaceId.NEW_TAB_EN_GB),
 
-        # Accept underscores in locale
-        ('en_US', None, RecommendationSurfaceId.NEW_TAB_EN_US),
-        ('en_GB', None, RecommendationSurfaceId.NEW_TAB_EN_GB),
-        ('de_DE', None, RecommendationSurfaceId.NEW_TAB_DE_DE),
-
         # locale can start/end with whitespace, and case can vary.
-        ('\r\n\t  eN_US \n ', None, RecommendationSurfaceId.NEW_TAB_EN_US),
-        ('\r\n\t  En_GB \n ', None, RecommendationSurfaceId.NEW_TAB_EN_GB),
-        ('\r\n\t  EN_ie \n ', None, RecommendationSurfaceId.NEW_TAB_EN_GB),
-        ('\r\n\t  en_IN \n ', None, RecommendationSurfaceId.NEW_TAB_EN_INTL),
+        ('eN-US', None, RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('En-GB', None, RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('EN-ie', None, RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('en-IN', None, RecommendationSurfaceId.NEW_TAB_EN_INTL),
 
         # region can start/end with whitespace, and case can vary.
-        ('en', ' \n US  \t', RecommendationSurfaceId.NEW_TAB_EN_US),
-        ('en', ' \n gB  \t', RecommendationSurfaceId.NEW_TAB_EN_GB),
-        ('en', ' \n Ie  \t', RecommendationSurfaceId.NEW_TAB_EN_GB),
-        ('en', ' \n in  \t', RecommendationSurfaceId.NEW_TAB_EN_INTL),
+        ('en', 'US', RecommendationSurfaceId.NEW_TAB_EN_US),
+        ('en', 'gB', RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('en', 'Ie', RecommendationSurfaceId.NEW_TAB_EN_GB),
+        ('en', 'in', RecommendationSurfaceId.NEW_TAB_EN_INTL),
 
         # Default to international NewTab when region is unknown.
         ('en', 'XX', RecommendationSurfaceId.NEW_TAB_EN_INTL),
