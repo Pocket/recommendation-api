eb2b1b39170f3ae1e343b45a71b763ec7a4c60af
Katerina Chinnappan <katchinnapp@gmail.com>
2023-11-21 15:57:41 -0800

    Katerina/mc 309 remove candidate set validation (#1070)
    * MC-309: removing candidate set validation check

--- app/main.py
+++ app/main.py
@@ -19,7 +19,6 @@ from strawberry.fastapi import GraphQLRouter
 from app.cache import initialize_caches
 from app.config import ENV, ENV_PROD, sentry as sentry_config, log_level, otel_daemon_address
 from app.graphql.graphql_router import schema
-from app.health_status import get_health_status, set_health_status, HealthStatus
 from app.instrumentation.aiobotocore import AiobotocoreInstrumentor
 from app.models.candidate_set import candidate_set_factory
 from app.models.slate_config import SlateConfigModel
@@ -67,11 +66,7 @@ AioHttpClientInstrumentor().instrument()
 
 @app.get("/health-check")
 async def read_root(response: Response):
-    if get_health_status() != HealthStatus.HEALTHY:
-        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-
-    return {"status": get_health_status().name}
-
+    return {"status": "HEALTHY"}
 
 class MissingSlateException(ValueError):
     """
@@ -104,37 +99,6 @@ async def load_slate_configs():
     SlateConfigModel.SLATE_CONFIGS_BY_ID = {s.id: s for s in slate_configs}
     slate_lineup_configs = SlateLineupConfigModel.load_slate_lineup_configs()
     SlateLineupConfigModel.SLATE_LINEUP_CONFIGS_BY_ID = {lc.id: lc for lc in slate_lineup_configs}
-
-    # Check for GUID re-use
-    validate_unique_guids(slate_lineup_configs, slate_configs)
-
-    # Validate slate_lineup and slate configs on prod, not locally or in dev.
-    if ENV == ENV_PROD:
-        # wow i do not love this nested loop soup, BUT it does give us nice full context for the error message
-        for slate_config in slate_configs:
-            for experiment in slate_config.experiments:
-                for cs in experiment.candidate_sets:
-                    logging.info(f"Validating candidate set {cs}")
-                    csm = candidate_set_factory(cs)
-                    if not await csm.verify_candidate_set(cs):
-                        # Send event to Sentry, but don't raise it, because missing candidate sets should not
-                        # block successfully starting the application.
-                        message = f'candidate set {slate_config.id}|{experiment.description}|{cs} was not found.'
-                        logging.error(message)
-                        sentry_sdk.capture_exception(MissingCandidateSetException(message))
-
-        for slate_lineup_config in slate_lineup_configs:
-            for experiment in slate_lineup_config.experiments:
-                for slate in experiment.slates:
-                    logging.info(f"Validating slate id {slate}")
-                    if not SlateLineupExperimentModel.slate_id_exists(slate):
-                        set_health_status(HealthStatus.UNHEALTHY)
-                        raise MissingSlateException(
-                            f'slate {slate_lineup_config.id}|{experiment.description}|{slate} was not found'
-                            f'in json/slate_configs.json - application start failed')
-
-    set_health_status(HealthStatus.HEALTHY)
-
 
 @app.on_event("startup")
 async def startup_event():
